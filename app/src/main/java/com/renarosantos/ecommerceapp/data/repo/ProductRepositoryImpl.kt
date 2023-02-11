d3e2226ad337ba39d5c8610160f62f193960902d
Josue Lubaki <josuelubaki@gmail.com>
2023-02-11 05:56:25 -0500

    added call of repository

--- app/src/main/java/com/renarosantos/ecommerceapp/data/repo/ProductRepositoryImpl.kt
+++ app/src/main/java/com/renarosantos/ecommerceapp/data/repo/ProductRepositoryImpl.kt
@@ -1,21 +1,22 @@
 package com.renarosantos.ecommerceapp.data.repo
 
+import com.renarosantos.ecommerceapp.data.remote.ApiClient
 import com.renarosantos.ecommerceapp.ui.ProductCardViewState
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 class ProductRepositoryImpl {
 
+    private val client = ApiClient().getClient()
+
     suspend fun getProductList(): List<ProductCardViewState> {
         return withContext(Dispatchers.IO) {
-            delay(2000)
-            (1..5).map {
+            client.getProductList().map {
                 ProductCardViewState(
-                    "Playstation $it",
-                    "This is a nice console! Check it out",
-                    "${200 * it} US$",
-                    "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    title = it.title,
+                    description = it.description,
+                    price = "${it.price.toInt()} CAD",
+                    imageUrl = it.imageUrl
                 )
             }
         }
