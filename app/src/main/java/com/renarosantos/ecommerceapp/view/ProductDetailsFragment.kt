f333b51fd764eec32b77b486d93a4049973a5f34
Josue Lubaki <josuelubaki@gmail.com>
2023-02-11 10:26:50 -0500

    added navigation

--- app/src/main/java/com/renarosantos/ecommerceapp/view/ProductDetailsFragment.kt
+++ app/src/main/java/com/renarosantos/ecommerceapp/view/ProductDetailsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.renarosantos.ecommerceapp.databinding.FragmentProductDetailsBinding
+import com.renarosantos.ecommerceapp.ui.ProductCardViewState
 
 
 class ProductDetailsFragment : Fragment() {
@@ -23,12 +24,15 @@ class ProductDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // get the product from the arguments via Serializable
+        val product = arguments?.getSerializable("product") as ProductCardViewState
+
         with(binding){
-            viewProductTitle.text = "Playstation 3"
-            Glide.with(requireContext()).load("https://firebasestorage.googleapis.com/v0/b/androidecommercesample.appspot.com/o/playstation_1.png?alt=media&token=1414f40e-23cf-4f44-b922-e12bfcfca9f3")
-                .into(viewProductImage)
-            binding.viewPrice.text = "250 US$"
-            binding.viewFullDescription.text = "The PlayStation 3 (PS3) is a home video game console developed by Sony Computer Entertainment. It is the successor to PlayStation 2, and is part of the PlayStation brand of consoles. It was first released on November 11, 2006, in Japan,[9] November 17, 2006, in North America, and March 23, 2007, in Europe and Australia.[10][11][12] The PlayStation 3 competed primarily against Microsoft's Xbox 360 and Nintendo's Wii as part of the seventh generation of video game consoles."
+            viewProductTitle.text = product.title
+            Glide.with(requireContext()).load(product.imageUrl).into(viewProductImage)
+            binding.viewPrice.text = product.price
+            binding.viewFullDescription.text = product.description
         }
     }
 }
